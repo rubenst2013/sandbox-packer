3d01504ef4b643fdc1254a1c39558fd2c1d4bf02
Ruben Steinbacher <rsteinbacher@hefti.be.gk-software.com>
2019-07-27 14:12:32 +0200

    Add kernel update provisioning step
    The timeouts and pauses are necessary to allow proper handling of the required VM reboot.

---
+++ scripts/kernel_update.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+add-apt-repository -y ppa:teejee2008/ppa
+
+# Update the existing system to the latest patch version before trying to install a newer kernel.
+apt-get update
+apt-get upgrade -qq -y
+
+apt-get install -y ukuu
+
+ukuu --check
+
+ukuu --install v5.1.16
