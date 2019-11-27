2e5791352d37bcf35aeb98c7b6cfe51341363f6c
Ruben Steinbacher <rsteinbacher@gk-software.com>
2019-11-27 22:15:38 +0100

    Explicitly create the cache folder for ukuu

--- scripts/kernel_update.sh
+++ scripts/kernel_update.sh
@@ -2,6 +2,8 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+kernel_version="v5.3.13"
+
 add-apt-repository -y ppa:teejee2008/ppa
 
 # Update the existing system to the latest patch version before trying to install a newer kernel.
@@ -9,7 +11,8 @@ apt-get update
 apt-get upgrade -qq -y
 
 apt-get install -y ukuu gcc make perl
+mkdir -p "/home/vagrant/.cache/ukuu/${kernel_version}/amd64"
 
 ukuu --check
 
-ukuu --install v5.3.13
+ukuu --install "${kernel_version}"
