49fd6ea53d96d4b5b69cd7ebef583d555467e9a8
Ruben Steinbacher <rsteinbacher@gk-software.com>
2019-12-01 21:19:52 +0100

    Update kernel to 5.3.14

--- scripts/kernel_update.sh
+++ scripts/kernel_update.sh
@@ -2,7 +2,7 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-kernel_version="5.3.13"
+kernel_version="5.3.14"
 
 # Move kernel update helper to its proper location
 mv /tmp/ubuntu-mainline-kernel.sh /usr/local/bin/
