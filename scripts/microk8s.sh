0ca8e72bbebf15d0f5c2f64b1f6851c94fdc7b19
Ruben Steinbacher <rsteinbacher@gk-software.com>
2019-12-01 21:17:46 +0100

    Move lines about setting snapd refresh time frame

--- scripts/microk8s.sh
+++ scripts/microk8s.sh
@@ -7,13 +7,14 @@ apt-get update
 apt-get upgrade -qq -y
 apt-get install -y snapd curl
 
+# Set auto refresh to a more convenient time frame, https://snapcraft.io/docs/keeping-snaps-up-to-date
+snap set system refresh.timer=fri5,23:00-01:00
+
 # Install microk8s
 # https://microk8s.io/
 # Zero-ops single node Kubernetes cluster for workstations and appliances
 snap install microk8s --classic --channel=1.15/stable
 
-# Set auto refresh to a more convenient time frame, https://snapcraft.io/docs/keeping-snaps-up-to-date
-snap set system refresh.timer=fri5,23:00-01:00
 
 microk8s.start
 microk8s.status --wait-ready
