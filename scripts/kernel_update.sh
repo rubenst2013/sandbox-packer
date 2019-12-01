b87977ecd2fc4036908c9c6ee29eb07a0096c36b
Ruben Steinbacher <rsteinbacher@gk-software.com>
2019-12-01 20:47:32 +0100

    Switch away from ukuu in preparation to move to newer Ubuntu distribution version
    Ever since Ubuntu 19, the creator of UKUU asks for a fee to use the tool in corporate environments

--- scripts/kernel_update.sh
+++ scripts/kernel_update.sh
@@ -2,17 +2,17 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-kernel_version="v5.3.13"
+kernel_version="5.3.13"
 
-add-apt-repository -y ppa:teejee2008/ppa
+# Move kernel update helper to its proper location
+mv /tmp/ubuntu-mainline-kernel.sh /usr/local/bin/
+chmod +x /usr/local/bin/ubuntu-mainline-kernel.sh
+
 
 # Update the existing system to the latest patch version before trying to install a newer kernel.
 apt-get update
 apt-get upgrade -qq -y
+apt-get install -y gcc make perl
 
-apt-get install -y ukuu gcc make perl
-mkdir -p "/home/vagrant/.cache/ukuu/${kernel_version}/amd64"
-
-ukuu --check
-
-ukuu --install "${kernel_version}"
+ubuntu-mainline-kernel.sh -c                        # Check for available (newer) kernel versions
+ubuntu-mainline-kernel.sh -i "${kernel_version}"    # Install specified mainline kernel
