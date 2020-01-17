51f34b2c961fa8d8981febc01baba96828b8109e
Ruben Steinbacher <rsteinbacher@gk-software.com>
2020-01-17 15:40:58 +0100

    Update kernel to v5.4.12

--- scripts/kernel_update.sh
+++ scripts/kernel_update.sh
@@ -2,7 +2,7 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-kernel_version="5.4.8"
+kernel_version="5.4.12"
 
 # Move kernel update helper to its proper location
 mv /tmp/ubuntu-mainline-kernel.sh /usr/local/bin/
