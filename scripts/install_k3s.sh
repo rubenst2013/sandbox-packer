8ea5eaa97a03bbedb9998e309822cabe4102f470
Ruben Steinbacher <rsteinbacher@gk-software.com>
2020-01-31 13:53:56 +0100

    Switch from microk8s to k3s
    - use new provisioning script for k3s, keep micro8s script around for reference
    - remove snapd

---
+++ scripts/install_k3s.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Install k3s
+# https://k3s.io
+# Zero-ops single node Kubernetes cluster for workstations and appliances
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--no-deploy=traefik --kube-apiserver-arg runtime-config=apps/v1beta1=true,apps/v1beta2=true,extensions/v1beta1/daemonsets=true,extensions/v1beta1/deployments=true,extensions/v1beta1/replicasets=true,extensions/v1beta1/networkpolicies=true,extensions/v1beta1/podsecuritypolicies=true" sh -s - &
+wait $!
+
+mkdir /home/.kube/
+mkdir /root/.kube/
+cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
+cp /etc/rancher/k3s/k3s.yaml /root/.kube/config
+
+chown vagrant:vagrant -R /home/vagrant/.kube/
