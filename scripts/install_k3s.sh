127ec0d27f48e9f5d66b179809080c377cf67208
Ruben Steinbacher <rsteinbacher@gk-software.com>
2020-01-31 17:21:16 +0100

    Pipe install script to bash explicitly
    On Debian-based systems sh points to Dash instead of Bash which can have weird side effects.

--- scripts/install_k3s.sh
+++ scripts/install_k3s.sh
@@ -5,7 +5,7 @@ export DEBIAN_FRONTEND=noninteractive
 # Install k3s
 # https://k3s.io
 # Zero-ops single node Kubernetes cluster for workstations and appliances
-curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--no-deploy=traefik --kube-apiserver-arg runtime-config=apps/v1beta1=true,apps/v1beta2=true,extensions/v1beta1/daemonsets=true,extensions/v1beta1/deployments=true,extensions/v1beta1/replicasets=true,extensions/v1beta1/networkpolicies=true,extensions/v1beta1/podsecuritypolicies=true" sh -s - &
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--no-deploy=traefik --kube-apiserver-arg runtime-config=apps/v1beta1=true,apps/v1beta2=true,extensions/v1beta1/daemonsets=true,extensions/v1beta1/deployments=true,extensions/v1beta1/replicasets=true,extensions/v1beta1/networkpolicies=true,extensions/v1beta1/podsecuritypolicies=true" bash -s - &
 wait $!
 
 wait 120 # Give k3s installer some extra time to finish
