1ab87152bf068f0f3d94d9118b8f133d78f5c31d
Ruben Steinbacher <rsteinbacher@gk-software.com>
2020-01-31 14:35:55 +0100

    Give k3s installer extra time to finish

--- scripts/install_k3s.sh
+++ scripts/install_k3s.sh
@@ -8,6 +8,8 @@ export DEBIAN_FRONTEND=noninteractive
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--no-deploy=traefik --kube-apiserver-arg runtime-config=apps/v1beta1=true,apps/v1beta2=true,extensions/v1beta1/daemonsets=true,extensions/v1beta1/deployments=true,extensions/v1beta1/replicasets=true,extensions/v1beta1/networkpolicies=true,extensions/v1beta1/podsecuritypolicies=true" sh -s - &
 wait $!
 
+wait 120 # Give k3s installer some extra time to finish
+
 mkdir /home/.kube/
 mkdir /root/.kube/
 cp /etc/rancher/k3s/k3s.yaml /home/vagrant/.kube/config
