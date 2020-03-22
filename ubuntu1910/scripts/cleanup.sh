a434bbebb839557cfd00c714722980890c225dfe
vanphuongma <vanphuong.ma@gmail.com>
2020-03-22 02:05:24 +0100

    Restructure repo to allow for multi-distribution

---
+++ ubuntu1910/scripts/cleanup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -eux
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Uninstall Ansible and remove PPA.
+apt -y remove --purge ansible
+apt-add-repository --remove ppa:ansible/ansible
+
+# Apt cleanup.
+apt autoremove
+apt update
+
+# Delete unneeded files.
+rm -f /home/vagrant/*.sh
+
+# Use fstrim to shrink the disks data files.
+fstrim --all --verbose || true
+
+
+# Add `sync` so Packer doesn't quit too early, before the large file is deleted.
+sync
+
+# Disable e2fsscrub entries in cron, causes high cpu load with kubernetes
+sed -i 's/^\([^#]\)/# \1/' /etc/cron.d/e2scrub_all
