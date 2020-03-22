9b3fdb54bad740f1fd175157f5039a9fc1c1b1b8
vanphuongma <vanphuong.ma@gmail.com>
2020-03-22 02:22:04 +0100

    Move disabling of e2fsscrub to ansible

--- debian10/scripts/cleanup.sh
+++ debian10/scripts/cleanup.sh
@@ -16,9 +16,5 @@ rm -f /home/vagrant/*.sh
 # Use fstrim to shrink the disks data files.
 fstrim --all --verbose || true
 
-
 # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
 sync
-
-# Disable e2fsscrub entries in cron, causes high cpu load with kubernetes
-sed -i 's/^\([^#]\)/# \1/' /etc/cron.d/e2scrub_all
