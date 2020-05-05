ad1a5804c80ba433c47011c739556f5f0a033b19
Ruben Steinbacher <ruben_steinbacher@yahoo.com>
2020-05-05 03:12:27 +0200

    Ubuntu 19.10 - Update kernel to 5.6.7

--- ubuntu1910/scripts/kernel_update.sh
+++ ubuntu1910/scripts/kernel_update.sh
@@ -2,7 +2,7 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-kernel_version="5.5.11"
+kernel_version="5.6.7"
 
 # Move kernel update helper to its proper location
 mv /tmp/ubuntu-mainline-kernel.sh /usr/local/bin/
