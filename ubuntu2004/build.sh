d8bebd7b2120ba8ddc7284df0a2c6a2638fd2085
Ruben Steinbacher <ruben_steinbacher@yahoo.com>
2020-07-13 01:30:06 +0200

    Update kernel to 5.7.8
    Update box version string

--- ubuntu2004/build.sh
+++ ubuntu2004/build.sh
@@ -2,4 +2,4 @@
 
 export PACKER_CACHE_DIR='/usr/local/share/packer/cache/' 
 
-packer build --var "version=0.1.0" --var "build_number=b02" --var "branch_name=master" box-config.json
+packer build --var "version=0.2.0" --var "build_number=b05" --var "branch_name=master" box-config.json
