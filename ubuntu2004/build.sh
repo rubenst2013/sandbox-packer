ec2203069b531c390f74588aa4a3bc4712795cbd
Ruben Steinbacher <ruben_steinbacher@yahoo.com>
2020-10-03 23:59:26 +0200

    Update kernel version to 5.8.13

--- ubuntu2004/build.sh
+++ ubuntu2004/build.sh
@@ -2,4 +2,4 @@
 
 export PACKER_CACHE_DIR='/usr/local/share/packer/cache/' 
 
-packer build --var "version=0.3.0" --var "build_number=b05" --var "branch_name=master" box-config.json
+packer build --var "version=0.3.0" --var "build_number=b06" --var "branch_name=master" box-config.json
--- ubuntu2004/scripts/kernel_update.sh
+++ ubuntu2004/scripts/kernel_update.sh
@@ -2,7 +2,7 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-kernel_version="5.8.11"
+kernel_version="5.8.13"
 
 # Move kernel update helper to its proper location
 mv /tmp/ubuntu-mainline-kernel.sh /usr/local/bin/
