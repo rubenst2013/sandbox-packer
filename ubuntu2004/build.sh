2d73b3b08e38b6b2fa3292added50289f0a6dc5e
Ruben Steinbacher <ruben_steinbacher@yahoo.com>
2021-01-16 21:57:00 +0100

    Update box version to 0.4.0-b04

--- ubuntu2004/build.sh
+++ ubuntu2004/build.sh
@@ -2,4 +2,10 @@
 
 export PACKER_CACHE_DIR='/usr/local/share/packer/cache/' 
 
-packer build --var "version=0.3.0" --var "build_number=b06" --var "branch_name=master" box-config.json
+version="0.4.0"
+build_number="b04"
+branch_name="master"
+
+packer build --var "version=${version}" --var "build_number=${build_number}" --var "branch_name=${branch_name}" box-config.json
+
+vagrant box add --name "rst/ubuntu-server-20.04-${version}-${build_number}" "builds/virtualbox-ubuntu-2004-${version}-${build_number}-${branch_name}.box"
