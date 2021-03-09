c448dab948c12c79cd083818346b3bde7359ba27
Ruben Steinbacher <ruben_steinbacher@yahoo.com>
2021-03-09 02:02:50 +0100

    Update Kernel to v5.10.15

--- ubuntu2004/build.sh
+++ ubuntu2004/build.sh
@@ -3,7 +3,7 @@
 export PACKER_CACHE_DIR='/usr/local/share/packer/cache/' 
 
 version="0.6.0"
-build_number="b01"
+build_number="b02"
 branch_name="master"
 
 packer build --var "version=${version}" --var "build_number=${build_number}" --var "branch_name=${branch_name}" box-config.json
